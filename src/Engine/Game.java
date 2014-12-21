fd7a55bc41fd5b00582ee1fb278bdd57f6467baa
minkcv <minkcv@gmail.com>
2014-12-21 16:13:31 -0700

    add code to set up glfw and initialize and update (loop) the game class add test code for opengl

---
+++ src/Engine/Game.java
@@ -0,0 +1,31 @@
+package Engine;
+
+import static org.lwjgl.opengl.GL11.*;
+
+
+public class Game {
+	public Game(){
+		
+	}
+	
+	public void init(){
+		
+	}
+	
+	public void update(){
+		
+	}
+	
+	public void draw(){
+		glColor3f(.5f, .5f, .5f);
+		glBegin(GL_QUADS);
+		{
+			glVertex2f(0f, 0f);
+			glVertex2f(0f, .5f);
+			glVertex2f(.5f, .5f);
+			glVertex2f(.5f, 0f);
+			
+		}
+		glEnd();
+	}
+}
