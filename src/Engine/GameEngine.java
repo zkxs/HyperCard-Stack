5688a372a052a8b36037c9d0f4b5c1339cbdc7bd
minkcv <minkcv@gmail.com>
2014-12-21 15:17:59 -0700

    add lwjgl3, update classpaths, and add project_notes.txt

---
+++ src/Engine/GameEngine.java
@@ -0,0 +1,19 @@
+package Engine;
+
+import org.lwjgl.Sys;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+
+public class GameEngine {
+	public static final int SCREEN_WIDTH = 800;
+	public static final int SCREEN_HEIGHT = 600;
+	public static final String WINDOW_TITLE = "HyperCard Stack";
+	
+	public static void main(String[] args) {
+		new GameEngine();
+	}
+	
+	public GameEngine(){
+		
+	}
+}
