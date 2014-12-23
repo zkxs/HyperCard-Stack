3b741b2e96c8aec50398442ce348cd7243b05353
Michael Ripley <zkxs00@gmail.com>
2014-12-22 18:35:18 -0600

    Documented Renderer interface

--- src/engine/renderer/Renderer.java
+++ src/engine/renderer/Renderer.java
@@ -7,9 +7,22 @@ public interface Renderer
 {	
 	// drawImage
 	// drawRect
-	
 	// etc
 	
+	/**
+	 * Set the drawing color
+	 * @param red red component
+	 * @param green green component
+	 * @param blue blue component
+	 */
 	public void setColor(float red, float green, float blue);
+	
+	/**
+	 * 
+	 * @param x x coordinate of upper left corner
+	 * @param y y coordinate of upper left corner
+	 * @param width width of rectangle
+	 * @param height height of rectangle
+	 */
 	public void fillRect(float x, float y, float width, float height);
 }
